5143cc4b8ae7950d4cbece979f3da597d3209287
Bruno Lima <bruno-amil@hotmail.com>
2020-11-05 11:39:57 -0300

    subindo imagens pro strapi

---
+++ plugins/wysiwyg/controllers/wysiwyg.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * wysiwyg.js controller
+ *
+ * @description: A set of functions called "actions" of the `wysiwyg` plugin.
+ */
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: () => {},
+};
